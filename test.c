b967c446b5096ff03c4835ae97f64a7d9f6cbe96
Jack Lu <jlu2025@gmail.com>
2018-10-25 02:08:25 -0400

    learned

--- test.c
+++ test.c
@@ -11,17 +11,25 @@ int main(){
     char * buffer = malloc(256);
     //printf("%ld\n", strlen(m));
 
-    int test = open(pathname, O_CREAT | O_RDWR, 0777);
+    int test = open(pathname, O_CREAT | O_WRONLY, 0777);
 
     printf("write = %ld\n", write(test, m, strlen(m)));
     printf("error %d\n", errno);
     perror("error");
+
+    close(test);
     
-    char * content = malloc( strlen(m) * sizeof(char));
-    int end = read(test, content, 5);
+    test = open(pathname, O_RDWR);
+    char * content = calloc( strlen(m), sizeof(char));
+    int end = read(test, content, strlen(m));
     printf("read = %d\n", end);
-    content[end] = '\0';
-    printf("content: %s\n", content);
+    printf("content: %s", content);
+
+    m = "ahhhhhhhhhhhhh\n";
+    write(test, m, strlen(m));
+
+    end = read(test, content, strlen(m));
+    printf("content: %s", content);
 
     printf("error %d\n", errno);
     perror("error");
